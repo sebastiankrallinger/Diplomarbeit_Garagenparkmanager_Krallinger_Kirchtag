50754085dec1dda1ccde0de1b0ed054278226817
sebastiankrallinger <sebastian.krallinger@htl-saalfelden.at>
2024-11-06 21:14:49 +0100

    add navifation between pages

--- Garagenparkmanager/garagenparkmanager.client/src/main.jsx
+++ Garagenparkmanager/garagenparkmanager.client/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './mainpage_user.css'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
