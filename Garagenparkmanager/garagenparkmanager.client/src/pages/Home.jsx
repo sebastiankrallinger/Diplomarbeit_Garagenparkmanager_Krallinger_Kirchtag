52f41a2a5466b4e68a6694f49d3c9e304442a22e
sebastiankrallinger <sebastian.krallinger@htl-saalfelden.at>
2024-11-07 22:32:44 +0100

    fix headers

--- Garagenparkmanager/garagenparkmanager.client/src/pages/Home.jsx
+++ Garagenparkmanager/garagenparkmanager.client/src/pages/Home.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import './Home.css';
-import Header from '../components/Header_LoadingPage';
+import Header from '../components/Header'
 import Footer from '../components/Footer';
 
 function Home() {
--- Garagenparkmanager/garagenparkmanager.client/src/pages/Mainpage_User.jsx
+++ Garagenparkmanager/garagenparkmanager.client/src/pages/Mainpage_User.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import './Mainpage_User.css';
-import Header from '../components/Header';
+import Header from '../components/Header_User';
 import Footer from '../components/Footer';
 
 function Mainpage_User() {
