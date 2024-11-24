363d0c794839bbf24b9e9956fad54d84417d60fa
sebastiankrallinger <sebastian.krallinger@htl-saalfelden.at>
2024-11-24 20:56:36 +0100

    try keycloak

--- Garagenparkmanager/Garagenparkmanager.Server/Program.cs
+++ Garagenparkmanager/Garagenparkmanager.Server/Program.cs
@@ -34,7 +34,6 @@ namespace Garagenparkmanager.Server
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
 
 
--- Garagenparkmanager/Garagenparkmanager.Server/obj/Debug/net8.0/Garagenparkmanager.Server.AssemblyInfo.cs
+++ Garagenparkmanager/Garagenparkmanager.Server/obj/Debug/net8.0/Garagenparkmanager.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Garagenparkmanager.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+532afec47f8252ccd7bff513fc037485c832027f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d3381d7864b6601cae419111e0f038d23e8e61e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Garagenparkmanager.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Garagenparkmanager.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
