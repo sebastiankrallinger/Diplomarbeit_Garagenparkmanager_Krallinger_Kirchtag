a46fc6693301f7fc3f12724e8c10deb934f0c8ac
sebastiankrallinger <sebastian.krallinger@htl-saalfelden.at>
2025-02-20 23:38:17 +0100

    Revert "add nullable"
    This reverts commit df7eb988e85ebfb75aaa871344364b7ec746b604.

--- Garagenparkmanager/Garagenparkmanager.Server/Models/Admin.cs
+++ Garagenparkmanager/Garagenparkmanager.Server/Models/Admin.cs
@@ -9,14 +9,14 @@ namespace Garagenparkmanager.Server.Models
         public string Id { get; set; } = string.Empty;
         [JsonProperty(PropertyName = "role")]
         public Role Role { get; set; }
-        public string? Firstname { get; set; }
+        public string Firstname { get; set; }
 
-        public string? Lastname { get; set; }
+        public string Lastname { get; set; }
 
-        public string? Email { get; set; }
+        public string Email { get; set; }
 
-        public string? Password { get; set; }
+        public string Password { get; set; }
         [JsonProperty(PropertyName = "salt")]
-        public string? Salt { get; set; }
+        public string Salt { get; set; }
     }
 }
