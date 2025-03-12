2e8018cf375136ed0c57b0d4fb4e02ac6e406921
sebastiankrallinger <sebastian.krallinger@htl-saalfelden.at>
2025-03-12 07:39:52 +0100

    edit header

--- Garagenparkmanager/Garagenparkmanager.Server/obj/Debug/net8.0/Garagenparkmanager.Server.AssemblyInfo.cs
+++ Garagenparkmanager/Garagenparkmanager.Server/obj/Debug/net8.0/Garagenparkmanager.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Garagenparkmanager.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dd2e439b2ccc8d02e7b5f89fd2f07070a9fcba2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b13c7959593693ffb97242b2094d39e68fcca0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Garagenparkmanager.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Garagenparkmanager.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
