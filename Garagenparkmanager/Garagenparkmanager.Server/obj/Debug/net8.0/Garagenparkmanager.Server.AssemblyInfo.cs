0d770f87b737da96acd8ef5774cf70e5421ddd85
sebastiankrallinger <sebastian.krallinger@htl-saalfelden.at>
2025-04-24 14:24:22 +0200

    fix bugs

--- Garagenparkmanager/Garagenparkmanager.Server/obj/Debug/net8.0/Garagenparkmanager.Server.AssemblyInfo.cs
+++ Garagenparkmanager/Garagenparkmanager.Server/obj/Debug/net8.0/Garagenparkmanager.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Garagenparkmanager.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36b256345bd42cd399ca7addd9ab7d8051be921e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba5bc1e2f55579c083aff5d0470c9ed189e97c8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Garagenparkmanager.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Garagenparkmanager.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
